c6a9401ca36badb2a0277afa92d3a55bf7798d05
Louka Mellano <72651190+theilook@users.noreply.github.com>
2025-03-19 10:39:08 +0100

    🩺 Refacto Calendar Manager

--- MyCalendar/src/main/com/tpnote/entities/enums/EventTypeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.tpnote.entities.enums;
-
-public enum EventTypeEnum {
-    RDV_PERSONNEL,
-    REUNION,
-    PERIODIQUE
-}
--- MyCalendar/src/main/com/tpnote/services/CalendarManager.java
+++ MyCalendar/src/main/com/tpnote/services/CalendarManager.java
@@ -1,7 +1,6 @@
 package com.tpnote.services;
 
 import com.tpnote.entities.Event;
-import com.tpnote.entities.enums.EventTypeEnum;
 import com.tpnote.entities.primitives.*;
 
 import java.time.LocalDateTime;
@@ -20,18 +19,11 @@ public class CalendarManager {
             .toList();
     }
 
-    public boolean conflit(Event e1, Event e2) {
-        LocalDateTime fin1 = e1.dateDebut.plusMinutes(e1.dureeMinutes);
-        LocalDateTime fin2 = e2.dateDebut.plusMinutes(e2.dureeMinutes);
-
-        if (e1.type.equals(EventTypeEnum.PERIODIQUE) || e2.type.equals(EventTypeEnum.PERIODIQUE)) {
-            return false; // Simplification abusive
-        }
-
-        return e1.dateDebut.date().isBefore(fin2) && fin1.isAfter(e2.dateDebut);
-    }
-
     public void afficherEvenements() {
         events.events.stream().map(Event::description).forEach(System.out::println);
+    }
+
+    public void ajouterEvent(Event e) {
+        events.addEvent(e);
     }
 }
